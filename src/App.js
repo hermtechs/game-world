a166ae065677e6aa7c01900e8279b0d05d4df57d
hermtechs <hermtechs@gmail.com>
2023-05-11 00:27:18 +0300

    implemented dark mode with chakra ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import { Button, ButtonGroup, Grid, GridItem } from "@chakra-ui/react";
-import logo from "./assets/Logo/logo.webp";
-import styles from "./styles/Navigation.module.css";
 import Navigation from "./components/Navigation";
 function App() {
   return (
